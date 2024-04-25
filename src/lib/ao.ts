f65500328663094b7eb3d063d170ddf950c2cec3
ankushKun <ankush4singh@gmail.com>
2024-04-25 22:33:12 +0530

    fixed scheduler

--- src/lib/ao.ts
+++ src/lib/ao.ts
@@ -1,2 +1,2 @@
 export const Module = "9afQ1PLf2mrshqCTZEzzJTR2gWaC9zNPnYgYEqg1Pt4";
-export const Scheduler = "TZ7o7SIZ06ZEJ14lXwVtng1EtSx60QkPy-kh-kdAXog";
+export const Scheduler = "_GQ33BkPtZrqxA84vM8Zk-N2aO0toNNu_C-l-rawrBA";
