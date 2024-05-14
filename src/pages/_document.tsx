f9e0e8c8cfb88e9e70e439bdd894dcba7d61587e
ankushKun <ankush4singh@gmail.com>
2024-05-15 00:46:56 +0530

    favicons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,6 +14,11 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap"
           rel="stylesheet"
         />
+
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/icon.svg" type="image/svg" />
+        <link rel="apple-touch-icon" href="/apple-icon.png" type="image/png" />
+        <title>LearnAO | Home</title>
       </Head>
       <body>
         <Main />
